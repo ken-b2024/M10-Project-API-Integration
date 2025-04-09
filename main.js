a69e39f3fd8b61a95a75438287b441231356a920
ken-b2024 <kenneth.brown.2404@gmail.com>
2025-04-08 22:18:39 -0400

    Project Update

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const pokeInput = document.getElementById('pokeInput');
-const pokeBtn = document.getElementById('pokeBtn');
 
 async function fetchPokemon(pokeName) {
     const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokeName}`
     try {
         const res = await fetch(apiUrl)
         const data = await res.json()
+        console.log(data)
         return (data)
     }catch(err) {
         console.log(err)
@@ -16,26 +15,32 @@ function showPokemon(pokemon) {
     const container = document.querySelector('#pokeDisplay')
     const pokeCard = document.createElement('div')
     pokeCard.className = 'card-info'
-
+    
     pokeCard.innerHTML = `
-        <h5>${pokemon.name}</h5>
-        <img src = "${pokemon.sprites.front_default}">
-        <p>${pokemon.abilities.map(ability => ability.ability.name).join(',')}</p>
+        <img class = 'pokemon-display' src = "${pokemon.sprites.front_default}">
+        <h2>${pokemon.name}</h2>
+        <p class='fs-3'>Abilities: ${pokemon.abilities.map(ability => ability.ability.name).join(',')}</p>
+        <p class='fs-3'>Stats: ${pokemon.stats.map(stats => stats.stat.name).join(',')}</p>
+        <p class='fs-3'>Types: ${pokemon.types.map(types => types.type.name).join(',')}</p>
     `;
     container.appendChild(pokeCard)
+    console.log(container)
 };
-
-pokeBtn.addEventListener('click', async (event) => {
-    event.preventDefault()
-    const name = pokeInput.value
-    if (name) {
-        const pokemon = await fetchPokemon(name);
-        if (pokemon) {
-            showPokemon(pokemon);
+document.addEventListener('DOMContentLoaded', (event) => {
+    const pokeInput = document.getElementById('pokeInput');
+    const pokeBtn = document.getElementById('pokeBtn');
+    pokeBtn.addEventListener('click', async (event) => {
+        event.preventDefault()
+        const name = pokeInput.value
+        if (name) {
+            const pokemon = await fetchPokemon(name);
+            if (pokemon) {
+                showPokemon(pokemon);
+            }else {
+                alert("Could not fetch that Pokemon")
+            }
         }else {
-            alert("Could not fetch that Pokemon")
+            alert("Not a valid pokemon. Try again")
         }
-    }else {
-        alert("Not a valid pokemon. Try again")
-    }
+})
 });
